37899cac0049e345da44aa25ddacb4c7a2663f6a
Marni <marni@falkvard.com>
2023-10-30 13:27:29 +0100

    Submission page with controller, view and database

---
+++ AcmeCorporation/AcmeCorporationWebsite/Controllers/SubmissionController.cs
@@ -0,0 +1,23 @@
+﻿using AcmeCorporationLibrary.Data;
+using AcmeCorporationLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcmeCorporationWebsite.Controllers
+{
+    public class SubmissionController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SubmissionController(ApplicationDbContext db)
+        {
+            _db = db;
+            
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<SubmissionModel> objSubmission = _db.Submission;
+            return View(objSubmission);
+        }
+    }
+}
