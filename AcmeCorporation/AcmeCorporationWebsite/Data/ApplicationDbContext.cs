1f6503a4377c52b2f70e648d291291c597d2fcb8
Marni <marni@falkvard.com>
2023-10-30 10:06:02 +0100

    Db connection

--- AcmeCorporation/AcmeCorporationWebsite/Data/ApplicationDbContext.cs
+++ AcmeCorporation/AcmeCorporationWebsite/Data/ApplicationDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace AcmeCorporationWebsite.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AcmeCorporationWebsite.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+        }
+        //DbSet<SerialNumber>
     }
 }
